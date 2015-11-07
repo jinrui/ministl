8c9adf55a54096aaf1f5904ba6d1a7a4c234e8c0
jinrui <1029828759@qq.com>
2015-11-08 01:50:51 +0800

    delete debug

--- Debug/Test/TestAllocator.d
+++
@@ -1,8 +0,0 @@
-Test/TestAllocator.d: ../Test/TestAllocator.cpp ../Test/TestAllocator.h \
-  ../Test/../alloc.h ../Test/../allocator.h
-
-../Test/TestAllocator.h:
-
-../Test/../alloc.h:
-
-../Test/../allocator.h:
